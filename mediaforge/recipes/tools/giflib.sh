d33691a355c99e945b1dd62aec4d7318986c3263
Mattias Carlsson <mattias.carlsson01@gmail.com>
2026-03-22 18:07:05 +0100

    fix: add PKG_FILENAME for giflib SourceForge redirect
    SourceForge /download URLs save as filename 'download' instead
    of the actual tarball name. Set PKG_FILENAME explicitly.

--- mediaforge/recipes/tools/giflib.sh
+++ mediaforge/recipes/tools/giflib.sh
@@ -1,6 +1,7 @@
 PKG_NAME="giflib"
 PKG_VERSION="${PKG_VERSION_GIFLIB:-5.2.2}"
 PKG_URL="https://sourceforge.net/projects/giflib/files/giflib-${PKG_VERSION}.tar.gz/download"
+PKG_FILENAME="giflib-${PKG_VERSION}.tar.gz"
 
 pkg_configure() {
   cd "$DISTDIR/giflib-${PKG_VERSION}" || die "Failed to cd to giflib"
